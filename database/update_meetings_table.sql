60e2c26017d6f5704c215b01c8cd05da05d61ee0
InduwaraSMPN <pasindunaduninduwara@gmail.com>
2025-04-16 21:40:51 +0530

    remove unnecessary files

--- database/update_meetings_table.sql
+++
@@ -1,4 +0,0 @@
--- Add columns to meetings table for tracking notification status
-ALTER TABLE `meetings` 
-ADD COLUMN `reminderSent` TINYINT(1) NOT NULL DEFAULT 0,
-MODIFY COLUMN `feedbackRequestSent` TINYINT(1) NOT NULL DEFAULT 0 AFTER `reminderSent`;
