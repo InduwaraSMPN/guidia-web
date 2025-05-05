2e61f46ad5ba5372d7ef824244acf34fe484853e
InduwaraSMPN <pasindunaduninduwara@gmail.com>
2025-05-05 07:16:09 +0530

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
-// https://vitejs.dev/config/
+
 export default defineConfig({
     plugins: [react(), sentryVitePlugin({
             org: "student-s5e",
